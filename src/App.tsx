19599e45faade81b4a366ee532c7ac2ab50ce47d
David Lambl <david.lambl@gmail.com>
2024-02-27 23:15:02 -0500

    Updated dependencies and integrated react-query
    The project's dependencies have been updated, with the addition of '@tanstack/react-query' and '@tanstack/eslint-plugin-query'. The 'react-query' library has been replaced by '@tanstack/react-query'.
    
    In the application code, a QueryClient from react-query has been instantiated and used in App.tsx. This change allows for better state management of asynchronous data fetching.
    
    EventDetails component now uses useQuery hook from react-query to fetch API data based on selected date. Loading state is handled and fetched data is displayed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TimeFlipCalendar from './components/TimeFlipCalendar';
 import EventDetails from './components/EventDetails';
 import './App.css';
 
+// Create a client
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+const queryClient = new QueryClient();
+
 const App: React.FC = () => {
 	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<TimeFlipCalendar />} />
-				<Route path="/event/:date" element={<EventDetails />} />
-			</Routes>
-		</Router>
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+		<QueryClientProvider client={queryClient}>
+			<Router>
+				<Routes>
+					<Route path="/" element={<TimeFlipCalendar />} />
+					<Route path="/event/:date" element={<EventDetails />} />
+				</Routes>
+			</Router>
+			{/* <ReactQueryDevtools initialIsOpen /> */}
+		</QueryClientProvider>
 	);
 };
 
--- src/components/EventDetails.tsx
+++ src/components/EventDetails.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 
 interface EventDetailsProps {
 	selectedDate?: Date | null;
 }
 
+async function fetchApiStatus() {
+	const res = await fetch(`https://ui.dev/api/courses/react-query/status`);
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+	return await res.json();
+}
+
 const EventDetails: React.FC<EventDetailsProps> = (props) => {
 	const { date } = useParams<{ date: string }>();
+	const dateDetailsQuery = useQuery({
+		queryKey: [props.selectedDate?.toDateString()],
+		queryFn: () => fetchApiStatus(),
+	});
 
+	if (dateDetailsQuery.isLoading) {
+		return <div>...</div>;
+	}
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+	const data = dateDetailsQuery.data;
 	return (
 		<div>
 			<p>{props.selectedDate?.toDateString() ?? date}</p>
-			{/* Fetch and display event details based on the selected date */}
+			<pre>{JSON.stringify(data)}</pre>
 		</div>
 	);
 };

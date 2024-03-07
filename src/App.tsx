40d0debde206f70fb72a99a53381676f8ec74ac2
David Lambl <david.lambl@gmail.com>
2024-03-06 23:52:11 -0500

    Add netlify-identity-widget dependency and implement user authentication in App component
    - Added "netlify-identity-widget" as a dependency in package.json
    - Imported netlifyIdentity from 'netlify-identity-widget' in App.tsx
    - Initialized netlifyIdentity on component mount using useEffect hook
    - Implemented user authentication functionality with login, logout, and displaying the current user's name

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import React, { useEffect } from 'react';
+import netlifyIdentity from 'netlify-identity-widget';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -10,9 +16,20 @@ import './App.css';
 const queryClient = new QueryClient();
 
 const App: React.FC = () => {
+	useEffect(() => {
+		netlifyIdentity.init();
+	}, []);
+
+	const user = netlifyIdentity.currentUser();
+
 	return (
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 		<QueryClientProvider client={queryClient}>
+			<div>
+				<button onClick={() => netlifyIdentity.open('login')}>Log In</button>
+				<button onClick={() => netlifyIdentity.logout()}>Log Out</button>
+				<p>{user && `Welcome, ${user}`}</p>
+			</div>
 			<Router>
 				<Routes>
 					<Route path="/" element={<TimeFlipCalendar />} />

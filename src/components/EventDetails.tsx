6caaf8ad875c4f77b7c241e2fd604daa09dbf6ad
David Lambl <david.lambl@gmail.com>
2024-04-16 23:48:07 -0400

    Updated EventDetails component and styles
    1. In `EventDetails.tsx`, a new function was added to provide a way to format event time.
    2. Also, changes were made to handle the case of no tasks for a day and also to clearly state the component is 'Loading' for better user experience.

--- src/components/EventDetails.tsx
+++ src/components/EventDetails.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
-import { DailyReport } from '../interfaces/timeTrackingModel';
+import { DailyReport, Day } from '../interfaces/timeTrackingModel';
 import { fetchData } from '../services/apiService';
 
 interface EventDetailsProps {
@@ -14,29 +14,49 @@ async function fetchDailyReport(day: string) {
   );
 }
 
+const TimeDisplay: React.FC<{ totalTime: number }> = (props) => {
+  function formatTime(totalSeconds: number) {
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+
+    // Pad the seconds with leading zero if less than 10
+    const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+    return `${minutes}:${formattedSeconds}`;
+  }
+
+  return <p>{formatTime(props.totalTime)} minutes</p>;
+}
+
 const EventDetails: React.FC<EventDetailsProps> = (props) => {
   const { date } = useParams<{ date: string }>();
-  const day: string =
-    props.selectedDate?.toISOString()?.slice(0, 10) ?? date ?? '2024-01-01';
+  const day: string = props.selectedDate?.toISOString()?.slice(0, 10) ?? date ?? '2024-01-01';
   const dateDetailsQuery = useQuery({
     queryKey: [day],
     queryFn: () => fetchDailyReport(day),
   });
 
   if (dateDetailsQuery.isLoading) {
-    return <h1>...</h1>;
+    return <h1>Loading...</h1>;
   }
 
   const data: DailyReport | undefined = dateDetailsQuery.data;
+
+  // Find the correct day's data
+  const dayData = data?.weeks[0].days.find((d: Day) => d.dateStr === day);
+
   return (
     <div>
       <h1>Tasks for {day}</h1>
-      {data ? (
-        <p>
-          {data?.weeks[0].days[3].tasksInfo.map((taskInfo, index: number) => (
-            <div key={index}>{taskInfo.task.name}</div>
+      {dayData && dayData.tasksInfo.length > 0 ? (
+        <ul>
+          {dayData.tasksInfo.map((taskInfo, index) => (
+            <li key={index}>
+              <h3>{taskInfo.task.name}</h3>
+              <TimeDisplay totalTime={taskInfo.totalTime} />
+            </li>
           ))}
-        </p>
+        </ul>
       ) : (
         <p>No tasks found for this date.</p>
       )}

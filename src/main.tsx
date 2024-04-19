c0d873dd9db871f0a1d0436ebbdd4f0c823bb5e6
David Lambl <david.lambl@gmail.com>
2024-04-18 23:51:46 -0400

    Add Chakra UI provider to root component
    Imported the ChakraProvider from Chakra UI in main.tsx. Wrapped the root App component within the ChakraProvider to apply Chakra UI styles and functionality throughout the application.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from "@chakra-ui/react"
 import App from './App.tsx';
 import './index.css';
 
@@ -13,7 +14,9 @@ async function startServiceWorkerInDev() {
 void startServiceWorkerInDev().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </React.StrictMode>
   );
 });

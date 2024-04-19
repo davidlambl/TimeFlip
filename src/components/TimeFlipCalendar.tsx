843d82348206f731ffe9f0354557c7ffe30896d4
David Lambl <david.lambl@gmail.com>
2024-04-19 00:32:12 -0400

    Add Chakra UI components and login functionality to TimeFlipCalendar.tsx

--- src/components/TimeFlipCalendar.tsx
+++ src/components/TimeFlipCalendar.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Button, FormControl, FormLabel, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from '@chakra-ui/react';
 import Calendar from 'react-calendar';
 import EventDetails from './EventDetails';
 import './TimeFlipCalendar.css';
@@ -9,6 +10,7 @@ type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 const TimeFlipCalendar: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const handleDateChange = (value: Value) => {
     setSelectedDate(value instanceof Date ? value : null);
@@ -21,6 +23,32 @@ const TimeFlipCalendar: React.FC = () => {
       </div>
       <div className="lower-half">
         <EventDetails selectedDate={selectedDate} />
+        <Button colorScheme='teal' onClick={onOpen}>Login</Button>
+
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Login</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              <FormControl>
+                <FormLabel>Username</FormLabel>
+                <Input placeholder='Username' />
+              </FormControl>
+              <FormControl mt={4}>
+                <FormLabel>Password</FormLabel>
+                <Input placeholder='Password' type='password' />
+              </FormControl>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button colorScheme='blue' mr={3} onClick={onClose}>
+                                Save
+              </Button>
+              <Button onClick={onClose}>Cancel</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
       </div>
     </>
   );

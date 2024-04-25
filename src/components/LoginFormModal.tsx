252c96e2c33217327b9b64ac2eac22d6847b485b
David Lambl <david.lambl@gmail.com>
2024-04-24 23:40:46 -0400

    Add authentication to LoginFormModal
    Updated the LoginFormModal to process user authentication. Implemented useState hooks for the username and password fields. Also, a onSubmit callback prop was added to handle the submission of the form. In TimeFlipCalendar, provided onSubmit handling logic to set auth tokens using AuthService.

--- src/components/LoginFormModal.tsx
+++ src/components/LoginFormModal.tsx
@@ -1,35 +1,45 @@
 import { Button, FormControl, FormLabel, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from '@chakra-ui/react';
-import React from "react";
+import React, {useState} from "react";
 
 type ModalProps = {
     isOpen: boolean;
     onClose: () => void;
+    onSubmit: (username: string, password: string) => void;
 };
 
-const LoginFormModal: React.FC<ModalProps> = ({ isOpen, onClose }) => (
-  <Modal isOpen={isOpen} onClose={onClose}>
-    <ModalOverlay />
-    <ModalContent>
-      <ModalHeader>Login</ModalHeader>
-      <ModalCloseButton />
-      <ModalBody pb={6}>
-        <FormControl>
-          <FormLabel>Username</FormLabel>
-          <Input placeholder='Username' />
-        </FormControl>
-        <FormControl mt={4}>
-          <FormLabel>Password</FormLabel>
-          <Input placeholder='Password' type='password' />
-        </FormControl>
-      </ModalBody>
-      <ModalFooter>
-        <Button colorScheme='blue' mr={3} onClick={onClose}>
-                    Save
-        </Button>
-        <Button onClick={onClose}>Cancel</Button>
-      </ModalFooter>
-    </ModalContent>
-  </Modal>
-);
+const LoginFormModal: React.FC<ModalProps> = ({ isOpen, onClose, onSubmit }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const save = () => {
+    onSubmit(username, password);
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay/>
+      <ModalContent>
+        <ModalHeader>Login</ModalHeader>
+        <ModalCloseButton/>
+        <ModalBody pb={6}>
+          <FormControl>
+            <FormLabel>Username</FormLabel>
+            <Input placeholder='Username' onChange={(e) => setUsername(e.target.value)} />
+          </FormControl>
+          <FormControl mt={4}>
+            <FormLabel>Password</FormLabel>
+            <Input placeholder='Password' type='password' onChange={(e) => setPassword(e.target.value)} />
+          </FormControl>
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme='blue' mr={3} onClick={save}>
+                Save
+          </Button>
+          <Button onClick={onClose}>Cancel</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
 
 export default LoginFormModal;
--- src/components/TimeFlipCalendar.tsx
+++ src/components/TimeFlipCalendar.tsx
@@ -4,6 +4,7 @@ import Calendar from 'react-calendar';
 import EventDetails from './EventDetails';
 import LoginFormModal from './LoginFormModal';
 import './TimeFlipCalendar.css';
+import { AuthTokenManager } from '../services/authService';
 import 'react-calendar/dist/Calendar.css';
 
 type ValuePiece = Date | null;
@@ -17,9 +18,15 @@ const TimeFlipCalendar: React.FC = () => {
     setSelectedDate(value instanceof Date ? value : null);
   };
 
+  const handleOnSubmit = (username: string, password: string) => {
+    AuthTokenManager.setToken(username, password)
+      .then(() => { onClose(); }) 
+      .catch(err => { console.error(err); });
+  };
+
   return (
     <>
-      <LoginFormModal isOpen={isOpen} onClose={onClose} />
+      <LoginFormModal isOpen={isOpen} onSubmit={handleOnSubmit} onClose={onClose} />
       <div className="upper-half">
         <Calendar onChange={handleDateChange} value={selectedDate} />
       </div>

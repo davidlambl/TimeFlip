53c64b6541bf91b69d7dd7fa650f059b4b0ff6f4
David Lambl <david.lambl@gmail.com>
2024-04-24 23:40:16 -0400

    Refactor auth service for better token management
    Move getToken, setToken, and removeToken methods into a single AuthTokenManager object to streamline and enhance token management. getToken now throws an error if no token is found instead of assigning a new one, while setToken takes in username and password parameters for better flexibility. Introduced the removal of authToken from local storage in removeToken method.

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,34 +1,37 @@
-// authService.ts
-export const getToken = async (): Promise<string | null> => {
-  let token = localStorage.getItem('authToken');
-  if (!token) {
-    await setToken();
-    token = localStorage.getItem('authToken');
+export const AuthTokenManager = {
+  getToken: (): string => {
+    const token = localStorage.getItem('authToken');
+    if (!token) {
+      throw new Error('No auth token found');
+    }
+    return token;
+  },
+
+  setToken: async (username: string, password: string): Promise<void> => {
+    try {
+      const response = await fetch(
+        'https://newapi.timeflip.io/api/auth/email/sign-in',
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            email: username,
+            password,
+          }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      if (!response.ok) throw new Error('Network response was not ok');
+      const token = response.headers.get('Token')!;
+      localStorage.setItem('authToken', token);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      throw error;
+    }
+  },
+
+  removeToken: (): void => {
+    localStorage.removeItem('authToken');
   }
-  return token;
 };
-export const setToken = async (): Promise<void> => {
-  let token: string;
-  try {
-    const response = await fetch(
-      'https://newapi.timeflip.io/api/auth/email/sign-in',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: '',
-          password: '',
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-    if (!response.ok) throw new Error('Network response was not ok');
-    token = response.headers.get('Token')!;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    throw error;
-  }
-  localStorage.setItem('authToken', token);
-};
-export const removeToken = (): void => localStorage.removeItem('authToken');

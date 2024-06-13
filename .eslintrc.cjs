7740b5ecc70274dcf35ee2f8073432dab3e5db1e
David Lambl <david.lambl@gmail.com>
2024-06-12 23:40:47 -0400

    Update ESLint configuration
    The ESLint configuration has been updated for improved code styling and error-checking. Adjusted TypeScript ESLint plugin settings and added 'vite.config.ts' to the list of ignored files for linting. The rest of the configuration parameters remain intact.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,35 +7,35 @@ module.exports = {
     },
     extends: [
         'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:@typescript-eslint/recommended-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
         'plugin:react/recommended',
         'plugin:react/jsx-runtime',
-		'plugin:react-hooks/recommended',
-		],
-	settings: {
-		react: {
-			version: 'detect',
-		},
-	},
-	ignorePatterns: ['dist', '.eslintrc.cjs', '**/mockServiceWorker.js'],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-		project: ['./tsconfig.json', './tsconfig.node.json'],
-		tsconfigRootDir: __dirname,
-	},
-	plugins: ['react', '@typescript-eslint', 'react-refresh'],
-	rules: {
-		'react-refresh/only-export-components': [
-			'warn',
-			{ allowConstantExport: true },
-			],
-		indent: ['error', 2, { SwitchCase: 1 }],
-		'no-tabs': 'error',
-	},
+        'plugin:react-hooks/recommended',
+    ],
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    ignorePatterns: ['dist', '.eslintrc.cjs', '**/mockServiceWorker.js', 'vite.config.ts'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['./tsconfig.json', './tsconfig.node.json'],
+        tsconfigRootDir: __dirname,
+    },
+    plugins: ['react', '@typescript-eslint', 'react-refresh'],
+    rules: {
+        'react-refresh/only-export-components': [
+            'warn',
+            {allowConstantExport: true},
+        ],
+        indent: ['error', 2, {SwitchCase: 1}],
+        'no-tabs': 'error',
+    },
 };
